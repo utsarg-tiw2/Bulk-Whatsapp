13fa4ac14eb78cd43e53067d62885576f6f92de0
Utsarg Kumar Tiwary <utsargtiwary41@gmail.com>
2026-06-27 12:40:14 +0530

    fix: import secure_filename in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
